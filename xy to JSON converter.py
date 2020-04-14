17694c15d969031bda1b83734752049e338012b1
Kevin Yang <45344788+kevin200617@users.noreply.github.com>
2020-04-14 13:29:45 -0400

    Add files via upload
    This program changes the outputted data from https://www.mobilefish.com/services/record_mouse_coordinates/record_mouse_coordinates.php into a usable JSON file that the DFT can use.

---
+++ xy to JSON converter.py
@@ -0,0 +1,57 @@
+import json
+
+
+xCord = 0
+yCord = 0
+
+
+coordinateFile = "C:\\Users\\kyguy\\Desktop\\Make\Projects\\Discrete Fourier Transforms in PDE\\Shrek\\Shrek xy outline - no face.txt"
+JSONfile="C:\\Users\\kyguy\\Desktop\\Make\\Projects\\Discrete Fourier Transforms in PDE\\My changes\\Fourier_Transform\\Shrek xy outline - no face.json"
+
+
+def readFile():
+    #read file
+    file = open(coordinateFile, "r")
+    lines = file.readlines()
+    file.close
+    
+    #look for patterns
+    for line in lines:
+        line = line.strip()
+        if line.find(",")!= -1: #if there is a comma at this line
+            
+            divider = line.find(",") #finding position of comma 
+            xCord = line.split(",")[0]
+            xCord = int (xCord)
+            #print (xCord)
+            
+            yCord = line.split(",")[1]
+            yCord = int (yCord)
+            #print (yCord)
+
+            dictionary = {
+                "x": xCord,
+                "y": yCord
+            }
+            
+            
+            with open(JSONfile) as json_file: 
+                data = json.load(json_file) 
+                
+                temp = data['drawing'] 
+                # appending data to emp_details  
+                temp.append(dictionary) 
+                
+            write_json(data)  
+            
+
+        else: 
+            print("no comma at"+ line)
+    #display result
+
+# function to add to JSON 
+def write_json(data): 
+    with open(JSONfile,'w') as f: 
+        json.dump(data, f, indent=4) 
+
+readFile()
